0fd93f28968544ed1d03ff47ae641d5bf3b30b88
Matheus Bonavite <matheusbonavite@gmail.com>
2021-11-15 15:23:08 -0300

    Adding SASS and making sure it works properly :D

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import { StrictMode } from 'react';
+import './assets/index.scss';
 
 const Application: React.FunctionComponent<{}> = () => {
     return (
