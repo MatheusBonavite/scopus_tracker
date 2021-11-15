4cd64204721ff52cdcfae1aeb880a31d8457ea3f
Matheus Bonavite <matheusbonavite@gmail.com>
2021-11-15 18:08:10 -0300

    Making sure prettier and eslint work and adding script for both

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import { StrictMode } from 'react';
 import './assets/index.scss';
 
-const Application: React.FunctionComponent<{}> = () => {
-    return (
-        <h1>My Scopus Tracker App!</h1>
-    );
-}
+const Application: React.FunctionComponent<Record<string, unknown>> = () => {
+    return <h1>My Scopus Tracker App!</h1>;
+};
 
 ReactDOM.render(
     <StrictMode>
         <Application />
     </StrictMode>,
-    document.getElementById("root")
+    document.getElementById('root')
 );
